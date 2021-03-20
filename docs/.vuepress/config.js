73b4304e4dba46ce2210caad98373537991fd34d
Re1one <j901207@gmail.com>
2021-03-21 00:55:44 +0900

    update:TIL

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,23 @@
-module.exports = {
+module.exports = 
+{
   title: 'VuePress Blog Example', // Title for the site. This will be displayed in the navbar.
   description: 'Just playing around',
+  base:'/uoM/',
   themeConfig: {
     sidebar: [
-      '/_posts/2018-11-7-frontmatter-in-vuepress.md',
+      {
+        title: 'TIL',   // required
+        path: '/TIL/',      // optional, link of the title, which should be an absolute path and must exist
+        sidebarDepth: 1,    // optional, defaults to 1
+        children: [
+          '/TIL/TIL-20210320',
+          '/TIL/TIL-20210321'
+        ]
+      },
     ],
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Github', link: 'http://github.com/Reone1' }
     ],
   }
-  // theme: '@vuepress/theme-blog',
-  // themeConfig: {
-    // Please keep looking down to see the available options.
-  // }
 }
