312a0ddf4b2f46f821d715182f475ce8d8b3eab7
Re1one <j901207@gmail.com>
2021-04-07 22:10:22 +0900

    update: sidebar autowriter

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,34 +1,13 @@
 const dirTree = require('./dirTree.js')
+const defaultPath = './docs'
+
 module.exports = 
 {
   title: 'uoM', // Title for the site. This will be displayed in the navbar.
   description: 'jaewon github pages for blog',
   base:'/uoM/',
   themeConfig: {
-    sidebar: [
-      {
-        title: 'TIL',   // required
-        path: '/TIL/',      // optional, link of the title, which should be an absolute path and must exist
-        sidebarDepth: 1,    // optional, defaults to 1
-        children: dirTree('TIL')
-      },
-      {
-        title: 'React',   // required
-        path: '/React/',      // optional, link of the title, which should be an absolute path and must exist
-        sidebarDepth: 1,    // optional, defaults to 1
-        children: dirTree('React')
-      },
-      {
-        title:'Nodejs',
-        path:'/Nodejs/',
-        children: dirTree('Nodejs')
-      },
-      { 
-        title: 'Database',
-        path:'/Database/',
-        children:dirTree('Database')
-      }
-    ],
+    sidebar: dirTree(defaultPath),
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Github', link: 'http://github.com/Reone1/uoM' }
--- docs/.vuepress/dirTree.js
+++ docs/.vuepress/dirTree.js
@@ -1,16 +1,37 @@
 const fs = require('fs');
-const defaultPath = './docs'
 
-const dirTree =  (dirname) => {
-  let path = defaultPath + `/${dirname}` 
-  let list = fs.readdirSync(path)
-  let result = [];  
-  for (let fileName of list) {
-    let stat = fs.statSync(path + `/${fileName}`)
-    if (stat.isFile() && !/^README/.test(fileName)) {
-      result.push(`/${dirname}/${fileName}`)
-    }
-  }
-  return result
+const isDir = (root, path) => {
+  return fs.statSync(root + `/${path}`).isDirectory()
 }
+
+const isFile = (root, path) => {
+  return fs.statSync(root + `/${path}`).isFile()
+}
+
+const readSideMenu = (path) => {
+  const dirs = fs.readdirSync(path).filter(dirName => {
+    return isDir(path, dirName)
+  })
+  return dirs.filter(dir => {
+    return dir !== '.vuepress' && dir !== 'src'
+  })
+}
+
+const makeTree = (path) => {
+  return fs.readdirSync(path).filter(dirName => {
+    return isFile(path, dirName) && !/^README/.test(dirName)
+  })
+}
+const dirTree = (defaultPath) => {
+  const menus = readSideMenu(defaultPath)
+  return menus.map(menu => {
+    const files = makeTree(defaultPath + `/${menu}`)
+    return {
+      title: menu,
+      path:`/${menu}/`,
+      children: files.map(fileName => `/${menu}/${fileName}`)
+    }
+  })
+}
+
 module.exports = dirTree
