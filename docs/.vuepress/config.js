54c744dfc162fb8edec9dde720bcabd958e8dde9
Reone1 <j901207@gmail.com>
2022-01-14 17:45:14 +0900

    add sitmap plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,10 +2,10 @@ const treeLoader = require("./dirTree.js");
 const defaultPath = "./docs";
 
 module.exports = {
-  title: "uoM", // Title for the site. This will be displayed in the navbar.
+  title: "uoM TechLog", // Title for the site. This will be displayed in the navbar.
   description: "jaewon github pages for blog",
-  base: "/uoM/",
-  plugins: [require("./plugins/dirTree")],
+  base: "/",
+  plugins: [["sitemap", { hostname: "https://reone1.github.com/" }]],
   smoothScroll: true,
   themeConfig: {
     sidebar: treeLoader.dirTree(defaultPath),
